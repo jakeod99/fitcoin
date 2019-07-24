f838d0aa54eb7531c722813c8b6904daedb24e75
Jake O'Donnell <jakeod99@gmail.com>
2019-07-24 16:58:01 -0400

    still cant find the page

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 // GET /api/
 router.get('/hey', (req, res) => {
-    res.send("API GET HEY")
+    res.send("API GET HEY");
+});
+
+router.get('/profile', (req, res) =>{
+    var path = require('path');
+    res.sendFile(path.resolve('/../../app/src/profile.html'));
 });
 
 module.exports = router;
