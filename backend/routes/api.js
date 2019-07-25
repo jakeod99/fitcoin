3f62303c0dea63733477ad6345720d2456dfa7e6
Nicole Kennedy <nlkennedy@mitre.org>
2019-07-25 14:53:58 -0400

    updated routes for js and css

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -6,6 +6,13 @@ router.get('/hey', (req, res) => {
     res.send("API GET HEY");
 });
 
+// CSS References
+router.get('/styles.css', (req, res) =>{
+    var path = require('path');
+    res.sendFile(path.resolve('../app/src/styles/styles.css'));
+});
+
+// HTML References
 router.get('/index', (req, res) =>{
     var path = require('path');
     res.sendFile(path.resolve('../app/src/html/index.html'));
@@ -24,6 +31,12 @@ router.get('/competitions', (req, res) =>{
 router.get('/inbox', (req, res) =>{
     var path = require('path');
     res.sendFile(path.resolve('../app/src/html/inbox.html'));
+});
+
+// JavaScript References
+router.get('/inbox.js', (req, res) =>{
+    var path = require('path');
+    res.sendFile(path.resolve('../app/src/js/inbox.js'));
 });
 
 module.exports = router;
