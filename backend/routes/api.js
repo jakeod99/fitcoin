eb1dd2e57fb29e0b82fc75c4554c35d9a3f8ba77
Nicole Kennedy <nlkennedy@mitre.org>
2019-07-25 13:14:18 -0400

    header setup for html

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -6,9 +6,24 @@ router.get('/hey', (req, res) => {
     res.send("API GET HEY");
 });
 
+router.get('/index', (req, res) =>{
+    var path = require('path');
+    res.sendFile(path.resolve('../app/src/html/index.html'));
+});
+
 router.get('/profile', (req, res) =>{
     var path = require('path');
     res.sendFile(path.resolve('../app/src/html/profile.html'));
+});
+
+router.get('/competitions', (req, res) =>{
+    var path = require('path');
+    res.sendFile(path.resolve('../app/src/html/competitions.html'));
+});
+
+router.get('/inbox', (req, res) =>{
+    var path = require('path');
+    res.sendFile(path.resolve('../app/src/html/inbox.html'));
 });
 
 module.exports = router;
