ef05cdf980c86c8691e5a6de4e62a1abb80c7401
Julia P-A <juliapa@mitre.org>
2019-07-25 20:48:31 -0400

    bet manager 3

--- app/src/js/betManager.js
+++ app/src/js/betManager.js
@@ -1,67 +1,65 @@
 (function() {
-    this.pendingBets = {};
-    this.acceptedBets = {};
-    this.ongoingBets = {};
-    this.recentBets = {};
     // import moment from 'moment';
     // const moment = require('moment');
     // moment().format();
     console.log('yup');
   const betManager = {
+    pendingBets:  {},
+    acceptedBets: {},
+    ongoingBets: {},
+    recentBets: {},
     //get betId
-    betManager() {
-      this.pendingBets = {};
-      this.acceptedBets = {};
-      this.ongoingBets = {};
-      this.recentBets = {};
-    //   const moment = require('moment');
-    //   moment().format();
-    //   console.log('yup');
-    },
+    // betManager() {
+    // //   const moment = require('moment');
+    // //   moment().format();
+    // //   console.log('yup');
+    // },
 
     makeBet(participants, wager, startDate, betId) {
       //participants is an array of all participants
     console.log(participants, wager, startDate, betId);
-    console.log(this.pendingBets);
-      this.pendingBets[betId] = {participants: participants, wager: wager, startDate: startDate};
+    console.log(betManager.pendingBets);
+      betManager.pendingBets[betId] = {participants: participants, wager: wager, startDate: startDate};
     },
     acceptBet(betId) {
         console.log('accepting');
-      Object.keys(this.pendingBets).forEach(bet => {
+      Object.keys(betManager.pendingBets).forEach(bet => {
         if (bet === betId) {
-          this.acceptedBets[betId] = this.pendingBets[betId];
+          betManager.acceptedBets[betId] = betManager.pendingBets[betId];
         }
       });
     },
     checkIfStarted(betId) {
         console.log('checking');
-      Object.values(this.acceptedBets).forEach(bet => {
+      Object.values(betManager.acceptedBets).forEach(bet => {
         if (bet.startDate.isToday()) {
-          this.ongoingBets[betId] = bet;
+          betManager.ongoingBets[betId] = bet;
           bet.participants.forEach(participant => {
-            this.ongoingBets[betId][steps][participant] = participant.steps; //get from the fitbit api
+            betManager.ongoingBets[betId][steps][participant] = participant.steps; //get from the fitbit api
           });
-          this.ongoingBets[betId][ttg] = moment().toNow(
+          betManager.ongoingBets[betId][ttg] = moment().toNow(
             bet.startDate.add(1, "d")
           );
         }
       });
     },
     checkIfRecent(betId) {
+        console.log('recent');
       //check if any ongoingBets have finished, if so, remove it and add it to recentBets
-      Object.values(this.ongoingBets).forEach(bet => {
+      console.log(betManager.ongoingBets);
+      Object.values(betManager.ongoingBets).forEach(bet => {
         if (bet.startDate.add(1, "d").isAfter(moment())) {
-          this.recentBets[betId][participants] = bet.participants;
-          this.recentBets[betId][wager] = bet.wager;
-          this.recentBets[betId][startDate] = bet.startDate;
-          delete this.ongoingBets[betId];
+          betManager.recentBets[betId][participants] = bet.participants;
+          betManager.recentBets[betId][wager] = bet.wager;
+          betManager.recentBets[betId][startDate] = bet.startDate;
+          delete betManager.ongoingBets[betId];
           //calculate winner and add that to the object
         }
       });
       //check if any recentBets started more than 48 hours ago, if so, remove it from recentBets
-      Object.values(this.recentBets).forEach(bet => {
+      Object.values(betManager.recentBets).forEach(bet => {
         if (bet.startDate.add(2, "d").isBefore(moment())) {
-          delete this.recentBets[betId];
+          delete betManager.recentBets[betId];
         }
       });
     }
@@ -69,5 +67,4 @@
 
   window.betManager = betManager;
 })();
-
-window.betManager.betManager();
+$('#competitions').ready(window.betManager.checkIfRecent);
