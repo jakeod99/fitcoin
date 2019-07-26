d5a902cbeb4f0edc3940dfccc7e82d31261fbf01
Jake O'Donnell <jakeod99@gmail.com>
2019-07-25 21:32:12 -0400

    user routing complete

---
+++ backend/routes/user/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+var users = [
+    {
+        "username": "Jake",
+        "avgSteps": 3000,
+        "pubKey": "0x0000000000",
+        "picFile": "jakepic.jpg"
+    },
+    {
+        "username": "Alex",
+        "avgSteps": 4000,
+        "pubKey": "0x0000000001",
+        "picFile": "alexpic.jpg"
+    },
+    {
+        "username": "Julia",
+        "avgSteps": 5000,
+        "pubKey": "0x0000000002",
+        "picFile": "profilepic.jpg"
+    },
+    {
+        "username": "Nicole",
+        "avgSteps": 6000,
+        "pubKey": "0x0000000003",
+        "picFile": "nicolepic.jpg"
+    }
+];
+
+router.get('/all', (req, res) =>{
+    res.json(users);
+});
+
+router.get('/:id', (req, res) =>{
+    id = req.params.id;
+    res.json(users.find( (user) =>{
+        return user.username == id;
+    }));
+});
+
+
+
+module.exports = router;
